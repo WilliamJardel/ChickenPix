cb56efcc7502cbc50b53de4b5fd661d2d27cfb7f
Daniel <danielandrade07@alu.ufc.br>
2026-08-20 21:41:10 -0300

    build: configuracao inicial do projeto com Spring Boot e Java 21

---
+++ src/test/java/br/com/chiken_pix_back/chikenpix/ChikenPixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.chiken_pix_back.chikenpix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChikenPixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
