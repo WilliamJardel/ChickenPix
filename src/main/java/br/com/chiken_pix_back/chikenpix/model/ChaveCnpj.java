cb42a5fc2370c00611c21f5dcb2f48b5392554b1
Kethillyn <naykethllyn@gmail.com>
2026-08-25 16:48:29 -0300

    feature/adicionando classe chave cnpj

---
+++ src/main/java/br/com/chiken_pix_back/chikenpix/model/ChaveCnpj.java
@@ -0,0 +1,31 @@
+package br.com.chiken_pix_back.chikenpix.model;
+
+public class ChaveCnpj implements ChavePix {
+    private String chave;
+
+    public ChaveCnpj() {}
+
+    public ChaveCnpj(String chave) {
+        this.chave = chave;
+    }
+
+    @Override
+    public String getChave(){
+        return this.chave;
+    }
+
+    @Override
+    public TipoChavePix getTipoChave(){
+        return TipoChavePix.CNPJ;
+    }
+
+    @Override
+    public boolean validar(){
+        if (this.chave == null) {
+        return false;
+    }
+        return new CNPJValidator().isEligible(this.chave);
+    }
+
+
+}
