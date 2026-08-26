73123b688e78ec2b27b3f5b2bf929edb2e3d983e
Daniel Profissional <danielprofissional@email.com>
2026-08-26 09:10:17 -0300

    feat: implementa o tratamento de ChaveNaoEncontradaException na GlobalExcetionHandler

--- src/main/java/br/com/chiken_pix_back/chikenpix/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/chiken_pix_back/chikenpix/exception/GlobalExceptionHandler.java
@@ -1,4 +1,18 @@
 package br.com.chiken_pix_back.chikenpix.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(ChaveNaoEncontradaException.class)
+    public ResponseEntity<String> tratarChaveNaoEncontrada(ChaveNaoEncontradaException exception) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(exception.getMessage());
+    }
 }
