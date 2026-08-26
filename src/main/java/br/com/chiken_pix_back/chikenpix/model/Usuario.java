926267e9c3e51bc7d05404d075d25568ab747cdc
Camila Santos <camilasantoss1302@gmail.com>
2026-08-26 20:46:22 -0300

    feat: adiciona atualizarSenha a classe Usuario

--- src/main/java/br/com/chiken_pix_back/chikenpix/model/Usuario.java
+++ src/main/java/br/com/chiken_pix_back/chikenpix/model/Usuario.java
@@ -14,9 +14,11 @@ public class Usuario {
         this.cpf = cpf;
         this.senha = senha;
         this.conta = conta;
+
+        //quando criar o pacote de exceções colocar uma pra senha passada não atender aos critérios
     }
 
-    public boolean validarSenha() {
+    public boolean validarSenha(String valor) {
         if(this.senha ==  null || this.senha.isBlank()) {
             return false;
         }
@@ -26,6 +28,12 @@ public class Usuario {
         //Nesse metodo primeiro verifica se a senha não está nula ou se o espaço está vazio ou cheio de espaços em branco,
         //depois eu uso a trim() pra retirar qualquer espaço que o usuário digite "sem querer" e uso p matches() para estabelecer o padrão da ssenha a ser definida
         // que é ter pelo menos uma legtra maiuscula, uma minuscula, um numero e um caractere especial e deve ter no minimo 8 caractes.
+    }
+    public void atualizarSenha(String valor) {
+        //if(!validarSenha(valor)){
+            // lançar uma exceção
+        //}
+        this.senha = senha;
     }
 
 }
