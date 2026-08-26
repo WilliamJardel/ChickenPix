6a04f74516ed5daea038d3fa0ff99c1d03712f41
William Jardel <william.jardel@alu.ufc.br>
2026-08-26 20:46:51 -0300

    feat: implementa lombok getter/setter e reestrutura logica LocalDateTime

--- src/main/java/br/com/chiken_pix_back/chikenpix/model/Transacao.java
+++ src/main/java/br/com/chiken_pix_back/chikenpix/model/Transacao.java
@@ -1,18 +1,36 @@
 package br.com.chiken_pix_back.chikenpix.model;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
 import java.time.LocalDateTime;
 
 public class Transacao {
+
+    @Getter
     private String id;
+    @Setter
+    @Getter
     private double valor;
+    @Getter
     private TipoTransacao tipo;
-    private static LocalDateTime dateTime;
+    @Getter
+    private LocalDateTime dateTime;
+    @Setter
+    @Getter
+    private StatusTransacao status;
+    @Getter
     private ContaBancaria origem;
+    @Getter
     private ContaBancaria destino;
 
-    public Transacao(double valor, ContaBancaria destino){
+    public Transacao(double valor, ContaBancaria origem, ContaBancaria destino, TipoTransacao tipo){
+        this.id = UUID.randomUUID().toString();
         this.valor = valor;
+        this.origem = origem;
         this.destino = destino;
+        this.tipo = tipo;
+        this.dateTime = LocalDateTime.now();
+        this.status = StatusTransacao.PENDENTE; // sempre vai ta pendente
     }
-
-
 }
