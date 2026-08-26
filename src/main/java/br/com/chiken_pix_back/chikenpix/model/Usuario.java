1c94e258f0c5d8c5c60bc727b9511d21ea110469
Camila Santos <camilasantoss1302@gmail.com>
2026-08-26 20:26:06 -0300

    feat: adiciona validarSenha a classe Usuario

--- src/main/java/br/com/chiken_pix_back/chikenpix/model/Usuario.java
+++ src/main/java/br/com/chiken_pix_back/chikenpix/model/Usuario.java
@@ -7,4 +7,25 @@ public class Usuario {
     private String cpf;
     private String senha;
     private ContaBancaria conta;
+
+    public Usuario(String nome, String email, String cpf, String senha, ContaBancaria conta) {
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+        this.senha = senha;
+        this.conta = conta;
+    }
+
+    public boolean validarSenha() {
+        if(this.senha ==  null || this.senha.isBlank()) {
+            return false;
+        }
+
+        return this.senha.trim().matches("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%^&+=!]).{8,}$");
+
+        //Nesse metodo primeiro verifica se a senha não está nula ou se o espaço está vazio ou cheio de espaços em branco,
+        //depois eu uso a trim() pra retirar qualquer espaço que o usuário digite "sem querer" e uso p matches() para estabelecer o padrão da ssenha a ser definida
+        // que é ter pelo menos uma legtra maiuscula, uma minuscula, um numero e um caractere especial e deve ter no minimo 8 caractes.
+    }
+
 }
