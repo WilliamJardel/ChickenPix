c4fe5794bd7dadf03f4ff3297f6386ab5e3c6359
Camila Santos <camilasantoss1302@gmail.com>
2026-08-26 00:01:07 -0300

    feat: adiciona os atributos

---
+++ src/main/java/br/com/chiken_pix_back/chikenpix/model/Usuario.java
@@ -0,0 +1,10 @@
+package br.com.chiken_pix_back.chikenpix.model;
+
+public class Usuario {
+    private int id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String senha;
+    private ContaBancaria conta;
+}
