7222a6277875e6f331ddbe466312ee4fb7be3114
William Jardel <william.jardel@alu.ufc.br>
2026-08-26 10:18:16 -0300

    feat: implementa classe e construtor transacao

--- src/main/java/br/com/chiken_pix_back/chikenpix/model/Transacao.java
+++ src/main/java/br/com/chiken_pix_back/chikenpix/model/Transacao.java
@@ -1,4 +1,18 @@
 package br.com.chiken_pix_back.chikenpix.model;
+import java.time.LocalDateTime;
 
 public class Transacao {
+    private String id;
+    private double valor;
+    private TipoTransacao tipo;
+    private static LocalDateTime dateTime;
+    private ContaBancaria origem;
+    private ContaBancaria destino;
+
+    public Transacao(double valor, ContaBancaria destino){
+        this.valor = valor;
+        this.destino = destino;
+    }
+
+
 }
