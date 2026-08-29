84d40db1c16b4a5d85c219c19e98cf5cba1d666b
Daniel Profissional <danielprofissional@email.com>
2026-08-29 16:29:21 -0300

    feat: adiciona e implementa validação ValorPixInvalido em realizarPix de PixService

--- src/main/java/br/com/chiken_pix_back/chikenpix/service/PixService.java
+++ src/main/java/br/com/chiken_pix_back/chikenpix/service/PixService.java
@@ -1,5 +1,7 @@
 package br.com.chiken_pix_back.chikenpix.service;
 
+import br.com.chiken_pix_back.chikenpix.exception.ChaveNaoEncontradaException;
+import br.com.chiken_pix_back.chikenpix.exception.ValorPixInvalidoException;
 import br.com.chiken_pix_back.chikenpix.model.Banco;
 import br.com.chiken_pix_back.chikenpix.model.ChavePix;
 import br.com.chiken_pix_back.chikenpix.model.ContaBancaria;
@@ -7,7 +9,12 @@ import br.com.chiken_pix_back.chikenpix.model.ContaBancaria;
 public class PixService {
     private Banco banco;
 
-    public void realizarPix(ContaBancaria origem, String chaveDestino, double value) {
+    public void realizarPix(ContaBancaria origem, String chaveDestino, double value) throws ValorPixInvalidoException {
+        if (value <= 0.00){
+            throw new ValorPixInvalidoException(
+              "Valor inválido para realizar Pix."
+            );
+        }
         ContaBancaria destino = banco.buscarConta(chaveDestino);
 
         origem.debitar(value);
