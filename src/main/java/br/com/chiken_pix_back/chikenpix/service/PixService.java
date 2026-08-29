e3f94f14720a99be780cea037051a7467e419ee4
Daniel Profissional <danielprofissional@email.com>
2026-08-29 16:23:40 -0300

    feat: adiciona e implemnta realizarPix no PixService

--- src/main/java/br/com/chiken_pix_back/chikenpix/service/PixService.java
+++ src/main/java/br/com/chiken_pix_back/chikenpix/service/PixService.java
@@ -5,5 +5,12 @@ import br.com.chiken_pix_back.chikenpix.model.ChavePix;
 import br.com.chiken_pix_back.chikenpix.model.ContaBancaria;
 
 public class PixService {
+    private Banco banco;
 
+    public void realizarPix(ContaBancaria origem, String chaveDestino, double value) {
+        ContaBancaria destino = banco.buscarConta(chaveDestino);
+
+        origem.debitar(value);
+        destino.creditar(value);
+    }
 }
