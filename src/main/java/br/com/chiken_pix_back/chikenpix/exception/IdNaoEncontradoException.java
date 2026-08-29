04776da1ec55d561cacd06ecd5b3029d241d7207
Camila Santos <camilasantoss1302@gmail.com>
2026-08-28 21:27:02 -0300

    feat: implementa IdNaoEncontradoException em removerUsuario na classe Banco

---
+++ src/main/java/br/com/chiken_pix_back/chikenpix/exception/IdNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package br.com.chiken_pix_back.chikenpix.exception;
+
+public class IdNaoEncontradoException extends Exception {
+    public IdNaoEncontradoException(String message) {
+        super(message);
+    }
+}
