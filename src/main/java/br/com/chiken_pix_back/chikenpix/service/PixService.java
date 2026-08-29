5f4c7ac0cf6cb98f4c533b96ca47c72bb599412c
Daniel Profissional <danielprofissional@email.com>
2026-08-29 16:22:43 -0300

    feat: Adiciona a PixService no pacote service

---
+++ src/main/java/br/com/chiken_pix_back/chikenpix/service/PixService.java
@@ -0,0 +1,9 @@
+package br.com.chiken_pix_back.chikenpix.service;
+
+import br.com.chiken_pix_back.chikenpix.model.Banco;
+import br.com.chiken_pix_back.chikenpix.model.ChavePix;
+import br.com.chiken_pix_back.chikenpix.model.ContaBancaria;
+
+public class PixService {
+
+}
