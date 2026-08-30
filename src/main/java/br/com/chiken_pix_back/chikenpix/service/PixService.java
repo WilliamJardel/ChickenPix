bf2e24acb16cd073eae2d048d6cd6e389c00cf0f
Daniel Profissional <danielprofissional@email.com>
2026-08-30 17:41:01 -0300

    refactor: remove imports inutilizados e muda a ordem de buscarConta e validação de saldo

--- src/main/java/br/com/chiken_pix_back/chikenpix/service/PixService.java
+++ src/main/java/br/com/chiken_pix_back/chikenpix/service/PixService.java
@@ -1,10 +1,8 @@
 package br.com.chiken_pix_back.chikenpix.service;
 
 import br.com.chiken_pix_back.chikenpix.exception.ChaveNaoEncontradaException;
-import br.com.chiken_pix_back.chikenpix.exception.SaldoInsuficienteException;
 import br.com.chiken_pix_back.chikenpix.exception.ValorPixInvalidoException;
 import br.com.chiken_pix_back.chikenpix.model.Banco;
-import br.com.chiken_pix_back.chikenpix.model.ChavePix;
 import br.com.chiken_pix_back.chikenpix.model.ContaBancaria;
 import org.springframework.stereotype.Service;
 
@@ -17,13 +15,15 @@ public class PixService {
     }
 
     public void realizarPix(ContaBancaria origem, String chaveDestino, double valor) {
-        ContaBancaria destino = banco.buscarConta(chaveDestino);
 
         if (valor <= 0.00){
             throw new ValorPixInvalidoException(
                     "Error: Valor inválido para realizar Pix."
             );
         }
+
+        ContaBancaria destino = banco.buscarConta(chaveDestino);
+
 
         if (destino == null){
             throw new ChaveNaoEncontradaException(
