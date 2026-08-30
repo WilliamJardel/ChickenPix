fcdafb4d0a374cc3d4cc83f0abcedae11bb86944
Kethillyn <naykethllyn@gmail.com>
2026-08-30 01:53:41 -0300

    feat: adicionando método getChave

--- src/main/java/br/com/chiken_pix_back/chikenpix/model/ChaveTelefone.java
+++ src/main/java/br/com/chiken_pix_back/chikenpix/model/ChaveTelefone.java
@@ -2,9 +2,15 @@ package br.com.chiken_pix_back.chikenpix.model;
 
 public class ChaveTelefone implements ChavePix {
     private String chave;
+
     public ChaveTelefone() {}
 
     public ChaveTelefone(String chave){
         this.chave=chave;
+    }
+
+    @Override
+    public String getChave() {
+        return chave;
     }
 }
