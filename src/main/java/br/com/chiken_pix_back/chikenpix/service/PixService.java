acfea5c48f69211173c55098c4c906425dfc6df0
Daniel Profissional <danielprofissional@email.com>
2026-08-30 13:43:35 -0300

    feat: implementa vevalidação de SaldoInsuficiente em realizarPix do PixService

--- src/main/java/br/com/chiken_pix_back/chikenpix/service/PixService.java
+++ src/main/java/br/com/chiken_pix_back/chikenpix/service/PixService.java
@@ -1,6 +1,7 @@
 package br.com.chiken_pix_back.chikenpix.service;
 
 import br.com.chiken_pix_back.chikenpix.exception.ChaveNaoEncontradaException;
+import br.com.chiken_pix_back.chikenpix.exception.SaldoInsuficienteException;
 import br.com.chiken_pix_back.chikenpix.exception.ValorPixInvalidoException;
 import br.com.chiken_pix_back.chikenpix.model.Banco;
 import br.com.chiken_pix_back.chikenpix.model.ChavePix;
@@ -9,7 +10,13 @@ import br.com.chiken_pix_back.chikenpix.model.ContaBancaria;
 public class PixService {
     private Banco banco;
 
-    public void realizarPix(ContaBancaria origem, String chaveDestino, double value) throws ValorPixInvalidoException, ChaveNaoEncontradaException {
+    public void realizarPix(ContaBancaria origem, String chaveDestino, double value) throws ValorPixInvalidoException, ChaveNaoEncontradaException, SaldoInsuficienteException {
+        if (origem.getSaldo() - value < 0){
+            throw new SaldoInsuficienteException(
+              "Error: Saldo insuficiente para realizar Pix."
+            );
+        }
+
         if (value <= 0.00){
             throw new ValorPixInvalidoException(
               "Valor inválido para realizar Pix."
