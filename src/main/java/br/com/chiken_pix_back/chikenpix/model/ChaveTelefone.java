9981a4d656e4fcb9311de92a109261a174619ea8
Kethillyn <naykethllyn@gmail.com>
2026-08-30 01:42:57 -0300

    feat: adicionando atributos e construtor no chave telefone

--- src/main/java/br/com/chiken_pix_back/chikenpix/model/ChaveTelefone.java
+++ src/main/java/br/com/chiken_pix_back/chikenpix/model/ChaveTelefone.java
@@ -2,4 +2,9 @@ package br.com.chiken_pix_back.chikenpix.model;
 
 public class ChaveTelefone implements ChavePix {
     private String chave;
+    public ChaveTelefone() {}
+
+    public ChaveTelefone(String chave){
+        this.chave=chave;
+    }
 }
