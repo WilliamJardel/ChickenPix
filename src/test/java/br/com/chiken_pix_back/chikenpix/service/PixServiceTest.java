0508d0000127815689d119838a2926aa1c953533
Daniel Profissional <danielprofissional@email.com>
2026-08-31 14:18:25 -0300

    feat: adiciona e implementa teste de RealizarPixComSucesso em PixServiceTest

---
+++ src/test/java/br/com/chiken_pix_back/chikenpix/service/PixServiceTest.java
@@ -0,0 +1,64 @@
+package br.com.chiken_pix_back.chikenpix.service;
+
+import br.com.chiken_pix_back.chikenpix.model.ChaveEmail;
+import br.com.chiken_pix_back.chikenpix.model.Banco;
+import br.com.chiken_pix_back.chikenpix.model.TipoChavePix;
+import br.com.chiken_pix_back.chikenpix.model.Usuario;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class PixServiceTest {
+
+    private Banco banco;
+    private PixService pixService;
+
+    @BeforeEach
+    void setUp() {
+        banco = new Banco();
+        pixService = new PixService(banco);
+    }
+
+    @Test
+    @DisplayName("Deve realizar um Pix para outra conta com sucesso")
+    void deveRealizarPixComSucesso(){
+        Usuario usuarioAutenticadoOrigem = new Usuario(
+                "Regis",
+                "regis@email.com",
+                "894.321.242-06",
+                "bancodedados",
+                null,
+                "82976099776"
+        );
+        usuarioAutenticadoOrigem.getConta().creditar(100);
+
+
+        Usuario usuarioAutenticadoDestino = new Usuario(
+                "Camila",
+                "camila@email.com",
+                "124.324.244-07",
+                "minecraft",
+                null,
+                "82976089345"
+        );
+
+        usuarioAutenticadoDestino.getConta()
+                .addChavePix(TipoChavePix.EMAIL, new ChaveEmail("camila@email.com"));
+
+        banco.addUsuario(usuarioAutenticadoDestino);
+
+        pixService.realizarPix(
+                usuarioAutenticadoOrigem.getConta(),
+                "camila@email.com",
+                40
+        );
+
+        assertThat(usuarioAutenticadoDestino.getConta().getSaldo()).isEqualTo(40);
+        assertThat(usuarioAutenticadoOrigem.getConta().getSaldo()).isEqualTo(60);
+    }
+}
