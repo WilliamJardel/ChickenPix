c2081f591cbf1e2aa01b3df8aff4f52c93044051
Daniel Profissional <danielprofissional@email.com>
2026-08-31 14:25:29 -0300

    feat: adiciona e implementa teste de naoDeveRealizarPixInvalido em PixServiceTest

--- src/test/java/br/com/chiken_pix_back/chikenpix/service/PixServiceTest.java
+++ src/test/java/br/com/chiken_pix_back/chikenpix/service/PixServiceTest.java
@@ -1,5 +1,6 @@
 package br.com.chiken_pix_back.chikenpix.service;
 
+import br.com.chiken_pix_back.chikenpix.exception.ValorPixInvalidoException;
 import br.com.chiken_pix_back.chikenpix.model.ChaveEmail;
 import br.com.chiken_pix_back.chikenpix.model.Banco;
 import br.com.chiken_pix_back.chikenpix.model.TipoChavePix;
@@ -60,5 +61,46 @@ public class PixServiceTest {
 
         assertThat(usuarioAutenticadoDestino.getConta().getSaldo()).isEqualTo(40);
         assertThat(usuarioAutenticadoOrigem.getConta().getSaldo()).isEqualTo(60);
+    }
+
+    @Test
+    @DisplayName("Não deve realiar pix com valor inválido")
+    void naoDeveRealizarPixInvalido(){
+        Usuario usuarioAutenticadoOrigem = new Usuario(
+                "Regis",
+                "regis@email.com",
+                "894.321.242-06",
+                "bancodedados",
+                null,
+                "82976099776"
+        );
+        usuarioAutenticadoOrigem.getConta().creditar(100);
+
+
+        Usuario usuarioAutenticadoDestino = new Usuario(
+                "Camila",
+                "camila@email.com",
+                "124.324.244-07",
+                "minecraft",
+                null,
+                "82976089345"
+        );
+
+        usuarioAutenticadoDestino.getConta()
+                .addChavePix(TipoChavePix.EMAIL, new ChaveEmail("camila@email.com"));
+
+        banco.addUsuario(usuarioAutenticadoDestino);
+
+        assertThatThrownBy(
+                () -> pixService.realizarPix(
+                        usuarioAutenticadoOrigem.getConta(),
+                        "camila@email.com",
+                        -40
+                )
+        )
+                .isInstanceOf(ValorPixInvalidoException.class)
+                .hasMessage("Error: Valor inválido para realizar Pix.");
+
+
     }
 }
