65fd00d4b4d7e4486a8ec01d234fd76eddbc417e
Daniel Profissional <danielprofissional@email.com>
2026-09-19 14:04:01 -0300

    refactor: altera atributos e conststrutor da classe Transacao

--- src/main/java/br/com/chiken_pix_back/chikenpix/model/Transacao.java
+++ src/main/java/br/com/chiken_pix_back/chikenpix/model/Transacao.java
@@ -1,18 +1,29 @@
 package br.com.chiken_pix_back.chikenpix.model;
+import lombok.Getter;
+
 import java.time.LocalDateTime;
+import java.util.UUID;
 
+@Getter
 public class Transacao {
-    private String id;
-    private double valor;
+    private final String id;
+    private final double valor;
     private TipoTransacao tipo;
-    private static LocalDateTime dateTime;
-    private ContaBancaria origem;
-    private ContaBancaria destino;
+    private StatusTransacao status;
+    private final LocalDateTime dateHora;
+    private final ContaBancaria origem;
+    private final ContaBancaria destino;
 
-    public Transacao(double valor, ContaBancaria destino){
-        this.valor = valor;
+    public Transacao(ContaBancaria origem, ContaBancaria destino, double valor, TipoTransacao tipo){
+        this.id = UUID.randomUUID().toString();
+        this.origem = origem;
         this.destino = destino;
+        this.valor = valor;
+        this.status = StatusTransacao.PENDENTE;
+        this.tipo = tipo;
+        this.dateHora = LocalDateTime.now();
     }
+
 
 
 }
