ce651a537171c41546fbd8bfe310b6bfaf5789b1
William Jardel <william.jardel@alu.ufc.br>
2026-09-19 21:28:07 -0300

    feat: implementa chave pix validation

---
+++ src/main/java/br/com/chiken_pix_back/chikenpix/model/ChavePixValidation.java
@@ -0,0 +1,57 @@
+package br.com.chiken_pix_back.chikenpix.model;
+
+import br.com.chiken_pix_back.chikenpix.exception.AleatoriaInvalidaException;
+import br.com.chiken_pix_back.chikenpix.exception.CNPJInvalidoException;
+import br.com.chiken_pix_back.chikenpix.exception.CPFInvalidoException;
+import br.com.chiken_pix_back.chikenpix.exception.EmailInvalidoException;
+import br.com.chiken_pix_back.chikenpix.exception.TelefoneInvalidoException;
+
+public class ChavePixValidation{
+
+    private ChavePixValidation() {} // classe utilitária, não deve ser instanciada
+
+    public static void validar(TipoChavePix tipo, String valor) {
+        if (valor == null || valor.isBlank()) {
+            throw new IllegalArgumentException("Valor da chave Pix não pode ser vazio.");
+        }
+
+        switch (tipo) {
+            case CPF -> validarCpf(valor);
+            case CNPJ -> validarCnpj(valor);
+            case EMAIL -> validarEmail(valor);
+            case TELEFONE -> validarTelefone(valor);
+            case ALEATORIA -> validarAleatoria(valor);
+        }
+    }
+
+    private static void validarCpf(String valor) {
+        if (!valor.trim().matches("^(\\d{11}|\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2})$")) {
+            throw new CPFInvalidoException("CPF inválido para chave Pix.");
+        }
+    }
+
+    private static void validarCnpj(String valor) {
+        if (!valor.trim().matches("^(\\d{14}|\\d{2}\\.\\d{3}\\.\\d{3}/\\d{4}-\\d{2})$")) {
+            throw new CNPJInvalidoException("CNPJ inválido para chave Pix.");
+        }
+    }
+
+    private static void validarEmail(String valor) {
+        if (!valor.trim().matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
+            throw new EmailInvalidoException("E-mail inválido para chave Pix.");
+        }
+    }
+
+    private static void validarTelefone(String valor) {
+        if (!valor.trim().matches("^\\+?\\d{10,13}$")) {
+            throw new TelefoneInvalidoException("Telefone inválido para chave Pix.");
+        }
+    }
+
+    private static void validarAleatoria(String valor) {
+        String uuidRegex = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$";
+        if (!valor.trim().matches(uuidRegex)) {
+            throw new AleatoriaInvalidaException("Chave aleatória inválida — deve ser um UUID.");
+        }
+    }
+}
